da5a3520a8f735b6c296272e366172d1bd632ca0
obulbuk07 <obulbuk07@gamil.com>
2026-07-29 05:22:50 +0200

    add lanyard

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from "./components/About"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
 import Background from "./components/Backgroung"
+import Lanyard from './components/Lanyard'
 
 function App() {
 
   return (
    <>
+        <Lanyard position={[0, 0, 20]} lanyardWidth={0.4} gravity={[0, -40, 0]} />
         <Navbar />
         <Home/>
         <About/>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import HomeInfo from "./HomeInfo";
 
+
 function Home(){
     return(
         <div className="mx-auto w-3/4 grid grid-cols-3 gap-10 my-16 text-white p-6">
+            
             <HomeInfo/>
-            <div className="w-70 h-100 bg-white rounded">IMAGE</div>
+            
         </div>
     )
 }
