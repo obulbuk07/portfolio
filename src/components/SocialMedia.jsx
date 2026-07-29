322cd054a29620733e9a4389f5834c02ccb6c79d
obulbuk07 <obulbuk07@gamil.com>
2026-07-29 10:33:16 +0200

    add style for social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,7 @@
 function SocialMedia({img, name, link}){
     return(
         <a href={link}>
-            <div className="glass text white flex items-center gap-4 p-4 ">
+            <div className=" socials hover:bg-purple-500 glass text white flex items-center gap-4 p-4 ">
                 <img className=" h-10" src={img} alt="" />
                 <p>{name}</p>
             </div>
