7c79eabd7f64b210e60c88e669895ff71a154f2a
obulbuk07 <obulbuk07@gamil.com>
2026-07-29 11:25:55 +0200

    Add some new hover animations

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,8 +1,8 @@
 function Skill({img, name}){
     return(
-        <div className="bg-black rounded py-4 px-4 flex flex-col justify-between glass">
-            <img className=" " src={img} alt="" />
-            <p className="text-gray-500 text-center mt-2" clas>{name}</p>
+        <div className="h-35 group relative flex flex-col items-center gap-2 rounded-2xl border border-gray-800 bg-black p-4 transition-all duration-300 hover:-translate-y-2 hover:border-purple-500 hover:shadow-[0_0_30px_rgba(168,85,247,0.3)]">
+            <img className=" h-16 w-16 transition-transform duration-300 group-hover:scale-110" src={img} alt="" />
+            <p className="text-center mt-2 text-gray-500 transition-colors duration-300 group-hover:text-white" clas>{name}</p>
         </div>
     )
 
